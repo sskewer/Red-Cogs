3ae46d335cc6df6b2b1c4b4062e6309df0b6ec0c
Skewer <57370513+sskewer@users.noreply.github.com>
2020-06-15 09:31:43 +0200

    Update welcomerolereaction.py

--- welcomerolereaction/welcomerolereaction.py
+++ welcomerolereaction/welcomerolereaction.py
@@ -5,15 +5,15 @@ import discord
 from redbot.core import commands
 
 
-#------------# CONFIG #------------#
+#----------------# CONFIG #----------------#
 
-role = 721988422041862195
-reaction = '<:fnit_gift:601709109955395585>'
-message = 721988332153733141
-welcome_channel = 603955376286728226
+role_id = 721988422041862195
+reaction_name = '<:fnit_gift:601709109955395585>'
+message_id = 721988332153733141
+welcome_channel_id = 603955376286728226
 welcome_message = ['{user}, benvenuto nel team No Sweat!', '{user}? Il team No Sweat ti stava aspettando!', 'Team No Sweat, finalmente anche {user} è qui con noi!']
 
-#----------------------------------#
+#------------------------------------------#
 
 
 BaseCog = getattr(commands, "Cog", object)
@@ -25,3 +25,11 @@ class WelcomeRoleReaction(BaseCog):
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user)
+        # Vars
+        role = get(user.guild.roles, id=role_id)
+        welcome_channel = get(user.guild.channels, id=welcome_channel_id)
+        # Role
+        if reaction.message.id != message_id:
+            return
+        if reaction.emoji == reaction_name:
+            await user.add_roles(role)
