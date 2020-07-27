962fd2253e1994f26ba3c5fa675cc76d078f5830
Skewer <57370513+sskewer@users.noreply.github.com>
2020-07-27 09:40:35 +0200

    Update lockserver.py

--- lockserver/lockserver.py
+++ lockserver/lockserver.py
@@ -18,11 +18,21 @@ class LockServer(BaseCog):
     async def lockserver(self, ctx):
         member = ctx.guild.get_member(ctx.message.author.id)
         everyonePermissions = ctx.guild.default_role.permissions
+        perms_on = discord.Permissions(send_messages=True)
+        perms_off = discord.Permissions(send_messages=False)
         
-        if everyonePermissions.send_messages === true:
-            await ctx.guild.default_role.edit(permissions=send_messages, reason=f"{member.display_name} ha bloccato il server")
+        if everyonePermissions.send_messages == True:
+            await ctx.guild.default_role.edit(permissions=perms_on, reason=f"{member.display_name} ha bloccato il server")
             embed=discord.Embed(description=f"Il server è ora bloccato per l'invio di messaggi.", color=discord.Color.red(), timestamp=datetime.datetime.utcnow())
             embed.set_author(name="|  Blocco Server", url=member.avatar_url)
             embed.set_footer(text=f"Richiesto da {member.display_name}")
+            await ctx.send(embed=embed)
+            await ctx.message.delete()
         else:
+            await ctx.guild.default_role.edit(permissions=perms_off, reason=f"{member.display_name} ha sbloccato il server")
+            embed2=discord.Embed(description=f"Il server è ora sbloccato per l'invio di messaggi.", color=discord.Color.green(), timestamp=datetime.datetime.utcnow())
+            embed2.set_author(name="|  Blocco Server", url=member.avatar_url)
+            embed2.set_footer(text=f"Richiesto da {member.display_name}")
+            await ctx.send(embed=embed2)
+            await ctx.message.delete()
     
