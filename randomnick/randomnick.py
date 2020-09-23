9033ccd86fd7e451d3359dee449e63c16fd4eef3
Skewer <57370513+sskewer@users.noreply.github.com>
2020-09-23 19:19:09 +0200

    Update randomnick.py

--- randomnick/randomnick.py
+++ randomnick/randomnick.py
@@ -1,8 +1,11 @@
 import asyncio
+import random
 
 from contextlib import suppress
 import discord
 from redbot.core import commands
+
+randomArray = ["Pescesecco", "Aura", "Cuorenero", "Ibrido", "Bananita", "Lusso", "Trog", "Calzamaglia", "Yonder", "Fiaba", "Belladonna", "Cacciatrice", "Alla Deriva", "Linea Rossa", "Soccorso in Corsa", "Mazza", "Ragnarok", "Carburo", "Teknica", "Zoey", "Valore", "Omega", "Visitatore", "Specialista di Missioni", "Signore della Ruggine", "Camminatore Lunare", "Viaggiatore Oscuro", "Agente Élite", "Mietitore", "Cavaliere Blu", "Cavaliere Reale", "Cavaliere Nero", "Milite Assalto Aereo", "Incursore Rinnegato", "Coniglio Mastino", "Coniglio Incursore", "Segugio da Guerra", "Trifoglio Verde", "Wukong", "Difensore Scarlatto", "Ranger dell’Amore", "Schiaccianoci", "Saccheggiatore Felice", "Artigliere allo Zenzero", "Milite Ghoul", "Agente Ribelle", "Percussore Sommitale", "Asso", "Bombardiere Reale", "Percussore Blu", "Havoc", "Pioniere", "Subcomandante", "Galassia", "Doppia Elica", "Frostbite", "Riflesso"];
 
 BaseCog = getattr(commands, "Cog", object)
 
@@ -11,27 +14,13 @@ class RandomNick(BaseCog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.channel.id == 454264582622412801 or message.channel.id == 591189981481926666:
-            print("AUTOPUBLISH: Message to publish detected")
-            bot_channel = message.guild.get_channel(603955376286728226)
-            embed = discord.Embed(title="**Annullare la Pubblicazione?**", color=discord.Colour.dark_red())
-            embed.description = f"Il [messaggio]({message.jump_url}) sarà presto pubblicato in {message.channel.mention}.\nVuoi forzare l'annullamento dell'operazione?"
-            msg = await bot_channel.send(embed=embed)
-            await msg.add_reaction("❌")
-
-            def check(payload):
-                return payload.user_id != self.bot.user.id and payload.message_id == msg.id and payload.emoji.name == "❌"
-
-            try:
-                await self.bot.wait_for('raw_reaction_add', check=check, timeout=60)
-                await msg.clear_reactions()
-                embed = discord.Embed(title="**Pubblicazione annullata**", color=0x07b43e)
-                await msg.edit(embed=embed)
-                print("AUTOPUBLISH: Message not published")
-            except asyncio.TimeoutError:
-                with suppress(discord.errors.NotFound, discord.errors.HTTPException):
-                    await message.publish()
-                await msg.delete()
-                print("AUTOPUBLISH: Message published")
+    @commands.guild_only()
+    @commands.command(aliases=["random"])
+    @commands.has_permissions(manage_nicknames=True)
+    async def randomnick(self, ctx: Context, user: discord.Member):
+        randomnick = random.choice(randomArray)
+        try:
+            await user.edit(nick=randomnick)
+            await ctx.send(f":crayon: Il nuovo nome generato casualmente per **{user.name}** è **{randomnick}**.")
+        except:
+            await ctx.send(f":crayon: Non riesco a generare casualmente un soprannome per **{user.name}**.")
