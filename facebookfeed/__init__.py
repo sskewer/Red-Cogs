c0adf2ca286e8f2020874696cd65274c87a65ce0
Skewer <57370513+sskewer@users.noreply.github.com>
2020-12-08 20:26:21 +0100

    Update __init__.py

--- facebookfeed/__init__.py
+++ facebookfeed/__init__.py
@@ -3,4 +3,4 @@ from .facebookfeed import FacebookFeed
 def setup(bot):
     feed = FacebookFeed(bot)
     bot.add_cog(feed)
-    bot.loop.create_task(daily.checker())
+    bot.loop.create_task(feed.checker())
