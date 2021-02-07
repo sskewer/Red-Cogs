2543ba70e320a4f1e6c0643944f5558b69df0836
Skewer <57370513+sskewer@users.noreply.github.com>
2021-02-07 16:24:40 +0100

    Update channelreminder.py

--- channelreminder/channelreminder.py
+++ channelreminder/channelreminder.py
@@ -1,25 +1,18 @@
-import asyncio
-import datetime
-from asyncio import sleep
-
 import discord
-from discord.ext import commands, tasks
+from contextlib import suppress
+from redbot.core import commands
 
-class Events(commands.Cog):
-    def __init__(self, bot):
+BaseCog = getattr(commands, "Cog", object)
+
+class ChannelReminder(BaseCog):
+  """Lasciare un reminder come ultimo messaggio del canale"""
+  
+  def __init__(self, bot: Red):
         self.bot = bot
-        self.loop.start()
-        self.message.start()
+        
+  @commands.Cog.listener()
+  async def on_message(self, message):
+        
+        #--------- Bug Traduzione ---------#
+        if message.channel.id == 674689662509514752:
     
-    @tasks.loop(hours = 1)
-    async def message(self):
-        channel = self.bot.get_channel(733067878030508173)
-        evento_natalizio = self.bot.get_channel(776579327315279931)
-        embed = discord.Embed(colour = discord.Colour.dark_green(), title = channel.guild.name, description = f"**Grandi novità! <a:rainbow_hype:740844953231556612> Corri a scoprirle sulla nuova categoria Role-Play! <:wumpus_gift:777473503058198558>**")
-        def is_bot(message):
-            return message.author.id == self.bot.user.id
-        await channel.purge(limit = 40, check = is_bot)
-        await channel.send(embed = embed)
-            
-def setup(bot):
-  bot.add_cog(Events(bot))
