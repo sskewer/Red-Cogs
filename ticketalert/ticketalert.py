5663e3e3bbecbad2dd17a6f20414e9956e01f090
Skewer <57370513+sskewer@users.noreply.github.com>
2021-02-07 15:50:18 +0100

    Update ticketalert.py

--- ticketalert/ticketalert.py
+++ ticketalert/ticketalert.py
@@ -12,13 +12,14 @@ class TicketAlert(BaseCog):
   @commands.Cog.listener()
   async def on_message(self, message):
     if message.channel.id == 683363814137266207:
-      channel = message.guild.get_channel(663742727225212928)
+      channel = message.guild.get_channel(807985160703180850)
       vindertech_role = message.guild.get_role(659513332218331155)
       referente_vindertech_role = message.guild.get_role(720221658501087312)
       embed = discord.Embed(description = f"[`Nuova richiesta di supporto per voi`]({message.jump_url})", color = discord.Colour.from_rgb(19, 123, 196))
       embed.set_footer(text = "Reagisci per prendere in carico la segnalazione")
       sent_message = await channel.send(content = f"{referente_vindertech_role.mention} {vindertech_role.mention}", embed = embed)
       await sent_message.add_reaction("✅")
+      await sent_message.pin(reason="Nuova richiesa di supporto")
       def check(reaction, user):
         return reaction.message.id == sent_message.id and user.id != self.bot.user.id and str(reaction.emoji) == "✅"
       reaction, member = await self.bot.wait_for('reaction_add', check=check)
@@ -26,6 +27,7 @@ class TicketAlert(BaseCog):
       await sent_message.edit(content = "", embed = embed)
       sent_message = await channel.fetch_message(sent_message.id)
       await sent_message.clear_reactions()
+      await sent_message.unpin(reason=f"Richiesta presa in carico da {member}")
 
 def setup(bot):
   bot.add_cog(TicketAlert(bot))
