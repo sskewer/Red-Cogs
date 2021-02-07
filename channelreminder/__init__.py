722094534dd32539270c3cd4088792e9fb94677b
Skewer <57370513+sskewer@users.noreply.github.com>
2021-02-07 16:25:20 +0100

    Update __init__.py

--- channelreminder/__init__.py
+++ channelreminder/__init__.py
@@ -1,8 +1,4 @@
-from redbot.core.bot import Red
-
 from .channelreminder import ChannelReminder
 
-def setup(bot: Red):
-    reminder = ChannelReminder(bot)
-    bot.add_cog(reminder)
-    bot.loop.create_task(reminder.checker())
+def setup(bot):
+    bot.add_cog(ChannelReminder(bot))
