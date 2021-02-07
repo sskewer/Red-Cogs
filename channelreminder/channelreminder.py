9d5f386a6664a0922e770e7c4db59c13d7016f78
Skewer <57370513+sskewer@users.noreply.github.com>
2021-02-07 16:27:31 +0100

    Update channelreminder.py

--- channelreminder/channelreminder.py
+++ channelreminder/channelreminder.py
@@ -8,11 +8,12 @@ class ChannelReminder(BaseCog):
   """Lasciare un reminder come ultimo messaggio del canale"""
   
   def __init__(self, bot: Red):
-        self.bot = bot
+    self.bot = bot
         
   @commands.Cog.listener()
   async def on_message(self, message):
-        
-        #--------- Bug Traduzione ---------#
-        if message.channel.id == 674689662509514752:
     
+    #--------- Bug Traduzione ---------#
+    if message.channel.id == 674689662509514752:
+      embed = discord.Embed(description = "", color = discord.Colour.from_rgb(19, 123, 196))
+      await message.channel.send(embed=embed)
