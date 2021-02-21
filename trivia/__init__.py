829472edc2d57536ce53d86da48bf9e9d41afce7
MettiusHyper <69057048+MettiusHyper@users.noreply.github.com>
2021-02-21 22:48:38 +0100

    Update __init__.py

--- trivia/__init__.py
+++ trivia/__init__.py
@@ -2,6 +2,6 @@ from redbot.core.bot import Red
 from .trivia import trivia
 
 def setup(bot : Red):
-    trivia = trivia(bot)
-    bot.add_cog(trivia)
-    bot.loop.create_task(trivia.checker())
+    quiz = trivia(bot)
+    bot.add_cog(quiz)
+    bot.loop.create_task(quiz.checker())
