75784af24a55cec71c59acba48018c8418ec7870
Skewer <57370513+sskewer@users.noreply.github.com>
2021-02-24 18:31:40 +0100

    Update trivia.py

--- trivia/trivia.py
+++ trivia/trivia.py
@@ -72,7 +72,7 @@ class trivia(commands.Cog):
         default_guild = {"questions": [], "score" : {}, "setup" : {"color" : "#1a80e4", "time" : 12, "channel" : 680459534463926294}, "reaction" : {}}
         self.config.register_global(**default_global)
         self.config.register_guild(**default_guild)
-        self.bot.loop.create_task(checker(self))
+        checker(self)
         
     #--------------# COMMANDS #--------------#
 
