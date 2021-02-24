d0a5b4a22bcc1585ca2e5ce5e80f24157bece2f7
MettiusHyper <69057048+MettiusHyper@users.noreply.github.com>
2021-02-24 18:59:01 +0100

    Update trivia.py

--- trivia/trivia.py
+++ trivia/trivia.py
@@ -215,7 +215,6 @@ class trivia(commands.Cog):
     #------------# EVENT #------------#
                             
     async def checker(self):
-        await self.bot.get_channel(454268474534133762).send("ciao")
         guild = self.bot.get_guild(454261607799717888)
         setup = await self.config.guild(guild).setup()
         time = setup["time"]
@@ -226,7 +225,6 @@ class trivia(commands.Cog):
             hop = now + datetime.timedelta(days = 1) - datetime.timedelta(hours = now.hour - time)
             post_time = datetime.datetime(hop.year, hop.month, hop.day, hop.hour)
         delta_time = post_time - now
-        await self.bot.get_channel(454268474534133762).send(str(delta_time))
         await sleep(delta_time.seconds)
         while True:
             await post(self, guild)
