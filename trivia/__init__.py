54d05551c0a61d726b525014f5fb62c548e21ee2
Skewer <57370513+sskewer@users.noreply.github.com>
2021-02-24 20:29:59 +0100

    Update __init__.py

--- trivia/__init__.py
+++ trivia/__init__.py
@@ -1,8 +1,4 @@
-from redbot.core.bot import Red
-
 from .trivia import trivia
 
-async def setup(bot: Red):
-    quiz = trivia(bot)
-    bot.add_cog(quiz)
-    bot.loop.create_task(quiz.checker())
+async def setup(bot):
+    bot.add_cog(trivia(bot))
