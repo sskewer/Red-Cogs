b76b8cf40a0b10b9f887e3b5f2c195109b8a435c
MettiusHyper <69057048+MettiusHyper@users.noreply.github.com>
2021-02-24 19:53:09 +0100

    Update trivia.py

--- trivia/trivia.py
+++ trivia/trivia.py
@@ -57,50 +57,52 @@ class trivia(commands.Cog):
 
     @commands.guild_only()
     @commands.command()
-    async def domanda(self, ctx : commands.Context, *, question : str):
+    async def domanda(self, ctx : commands.Context):
         allowed_roles = [536214242685091860, 454268394464870401, 454262524955852800, 454262403819896833]
         for n, role in enumerate(allowed_roles):
             role = ctx.guild.get_role(role)
             allowed_roles[n] = role
         if len(set(ctx.author.roles).intersection(set(allowed_roles))) > 0:
-            await ctx.send("Qual è la risposta corretta?")
             def check(m):
                 return m.author == ctx.author and m.channel == ctx.channel
+            await ctx.send("Qual è la **domanda**?")
+            question = await self.bot.wait_for('message', check=check)
+            if len(question.content) > 256:
+                return await ctx.send("La **lughezza massima** per la domanda (256 caratteri) è stata superata, riprovare!")
+            
+            await ctx.send("Qual è la **risposta corretta**?")
             correct_answer = await self.bot.wait_for('message', check=check)
 
-            await ctx.send("Scrivi ora le risposte errate, separate da una `,`")
+            await ctx.send("Scrivi ora le **risposte errate**, separate da una `,`")
             incorrect_answers_raw = await self.bot.wait_for('message', check=check)
             incorrect_answers = incorrect_answers_raw.content.split(",")
             for n, answer in enumerate(incorrect_answers):
                 incorrect_answers[n] = answer.strip()
             question = {
-                "question" : question,
+                "question" : question.content,
                 "correct_answer" : correct_answer.content,
                 "incorrect_answers" : incorrect_answers
             }
-            await ctx.send("Invia ora l'immagine, rispondi con `No` se vuoi che la domanda non contenga alcuna immagine.")
+            await ctx.send("Invia ora l'immagine come **allegato**, altrimenti rispondi `No`.")
             image = await self.bot.wait_for('message', check=check)
-            if image.content != "No":
-                if image.attachments != []:
-                    question.update({"image" : image.attachments[0].url})
-                else:
-                    question.update({"image" : image.content})
+            if image.attachments != []:
+                question.update({"image" : image.attachments[0].url})
 
             user_incorrect = ""
             for incorrect in question["incorrect_answers"]:
-                user_incorrect += f"{incorrect}\n"
+                user_incorrect += f"• {incorrect}\n"
             setup = await self.config.guild(ctx.guild).setup()
             hex_int = int(setup["color"].replace("#", "0x"), 16)
             embed = discord.Embed(
-                description = f"**{question['question']}**", color = hex_int
+                title = question['question'], color = hex_int
             )
-            embed.add_field(name = "Risposta Corretta", value = question["correct_answer"])
-            embed.add_field(name = "Risposte Errate", value = user_incorrect)
+            embed.add_field(name = "Risposta Corretta", value = f"• {question['correct_answer']}", inline = False)
+            embed.add_field(name = "Risposte Errate", value = user_incorrect, inline = False)
             try:
                 embed.set_image(url = question["image"])
             except:
                 pass
-            msg = await ctx.send(content = "Reagisci con <:FNIT_ThumbsUp:454640434380013599> per aggiungere la domanda", embed = embed)
+            msg = await ctx.send(content = "Reagisci con <:FNIT_ThumbsUp:454640434380013599> per **aggiungere la domanda**", embed = embed)
             await msg.add_reaction("<:FNIT_ThumbsUp:454640434380013599>")
             await msg.add_reaction("<:FNIT_ThumbsDown:454640434610700289>")
             
@@ -111,7 +113,7 @@ class trivia(commands.Cog):
                 questions = await self.config.guild(ctx.guild).questions()
                 questions.append(question)
                 await self.config.guild(ctx.guild).questions.set(questions)
-                await ctx.send("Domanda aggiunta!")
+                await msg.edit(content = "Domanda **aggiunta** con successo!")
             await msg.clear_reactions()
             
     @commands.group(name="trivia")
@@ -123,16 +125,17 @@ class trivia(commands.Cog):
     @trivia.command()
     async def force_post(self, ctx: commands.Context):
         """Posta forzatamente il quiz"""
-        allowed_roles = [454262524955852800, 454262403819896833]
+        allowed_roles = [454262524955852800, 454262403819896833, 454268394464870401]
         for n, role in enumerate(allowed_roles):
             role = ctx.guild.get_role(role)
             allowed_roles[n] = role
         if len(set(ctx.author.roles).intersection(set(allowed_roles))) > 0:
             await post(self, ctx.guild)
+            await ctx.message.add_reaction("✅")
     
     @trivia.command(aliases = ["lb"])
     async def leaderboard(self, ctx: commands.Context):
-        allowed_roles = [454262524955852800, 454262403819896833]
+        allowed_roles = [454262524955852800, 454262403819896833, 454268394464870401]
         for n, role in enumerate(allowed_roles):
             role = ctx.guild.get_role(role)
             allowed_roles[n] = role
@@ -142,10 +145,30 @@ class trivia(commands.Cog):
             hex_int = int(setup["color"].replace("#", "0x"), 16)
             description = ""
             for n, el in enumerate(score):
-                description += f"**{n + 1}.** {ctx.guild.get_member(el)}(`{el}`)\n"
+                description += f"**{n + 1}.** {ctx.guild.get_member(int(el))}(`{el}`)\n"
             await ctx.send(embed = discord.Embed(
-                title = "LeaderBoard",description = description.strip(), color = hex_int
+                title = "Leaderboard", description = description.strip(), color = hex_int
             ).set_footer(text = ctx.guild.name, icon_url = ctx.guild.icon_url))
+    
+    @trivia.command()
+    async def leaderboard_reset(self, ctx: commands.Context):
+        allowed_roles = [454262524955852800, 454262403819896833]
+        for n, role in enumerate(allowed_roles):
+            role = ctx.guild.get_role(role)
+            allowed_roles[n] = role
+        if len(set(ctx.author.roles).intersection(set(allowed_roles))) > 0:
+            msg = await ctx.send(":warning: Sicuro di procedere?")
+            await msg.add_reaction("✅")
+            def reaction_check(reaction, user):
+                return user.id == ctx.message.author.id and str(reaction.emoji) == "✅" and reaction.message.id == msg.id
+            try:
+                reaction, user = await self.bot.wait_for('reaction_add', check=reaction_check, timeout=60.0)
+                await self.config.guild(ctx.guild).score.set({})
+                await msg.delete()
+                await ctx.message.add_reaction("✅")
+            except asyncio.TimeoutError:
+                await msg.delete()
+                await ctx.message.add_reaction("🚫")
 
     @trivia.command()
     async def current(self, ctx: commands.Context):
@@ -162,7 +185,7 @@ class trivia(commands.Cog):
             embed.add_field(name = "Time", value = f"{setup['time']}:00", inline = True)
             embed.add_field(name = "Channel", value = f"<#{setup['channel']}>", inline = True)
             embed.set_footer(text = ctx.guild.name, icon_url = ctx.guild.icon_url)
-            await ctx.channel.send(embed = embed)
+            await ctx.send(embed = embed)
     
     @trivia.command()
     async def color(self, ctx: commands.Context, value):
