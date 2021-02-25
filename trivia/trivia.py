19594f0de170ba913bd9abcba2f2ed2789582d54
MettiusHyper <69057048+MettiusHyper@users.noreply.github.com>
2021-02-25 19:49:36 +0100

    Update trivia.py

--- trivia/trivia.py
+++ trivia/trivia.py
@@ -232,7 +232,7 @@ class trivia(BaseCog):
    
     @trivia.command()
     async def cleardb(self, ctx: commands.Context):
-        """Resettare l2 domande"""
+        """Resettare le domande"""
         allowed_roles = [454262524955852800, 454262403819896833]
         for n, role in enumerate(allowed_roles):
             role = ctx.guild.get_role(role)
