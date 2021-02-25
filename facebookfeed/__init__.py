334387e4e3d6a532e2e48767580910fde2910614
Skewer <57370513+sskewer@users.noreply.github.com>
2021-02-25 19:45:52 +0100

    Update __init__.py

--- facebookfeed/__init__.py
+++ facebookfeed/__init__.py
@@ -1,8 +1,4 @@
-from redbot.core.bot import Red
-
 from .facebookfeed import FacebookFeed
 
-def setup(bot: Red):
-    feed = FacebookFeed(bot)
-    bot.add_cog(feed)
-    bot.loop.create_task(feed.checker())
+def setup(bot):
+    bot.add_cog(FacebookFeed(bot))
