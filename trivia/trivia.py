be5e2b2b84fe1356ed2bcbcd49613ffcddca5dbb
Skewer <57370513+sskewer@users.noreply.github.com>
2021-03-02 08:39:44 +0100

    Update trivia.py

--- trivia/trivia.py
+++ trivia/trivia.py
@@ -644,7 +644,7 @@ class trivia(BaseCog):
         checking = await guild.get_channel(816212393922658306).send(f"[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}] Controllando eventuali quiz terminati...")
         if datetime.datetime.fromtimestamp(reaction['time']) < datetime.datetime.now():
             await close(self)
-            await checking.channel.send(content = f"[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}] Quiz terminato in <#{str(setup['channel'])}>")
+            await checking.channel.send(content = f"[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}] Quiz terminato in <#{str(setup['channel'])}>\n<https://discord.com/channels/454261607799717888/{str(setup['channel'])}/{str(reaction['message'])}>")
         else:
             await checking.channel.send(f"[{time.strftime('%H:%M:%S', time.gmtime(time.time()))}] Nessun quiz terminato trovato")
                     
