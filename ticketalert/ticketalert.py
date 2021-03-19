aeff43799026c36ff9ab192a17eef30acdc0152a
Skewer <57370513+sskewer@users.noreply.github.com>
2021-03-19 22:49:04 +0100

    Update ticketalert.py

--- ticketalert/ticketalert.py
+++ ticketalert/ticketalert.py
@@ -26,16 +26,16 @@ class TicketAlert(BaseCog):
       
   @commands.Cog.listener()
   async def on_raw_reaction_add(self, payload : discord.RawReactionActionEvent):
-      guild = self.bot.get_guild(454261607799717888)
-      member = guild.get_user(payload.user_id)
-      channel = guild.get_channel(payload.channel_id)
-      message = await channel.fetch_message(payload.message_id)
-      if member.bot == False and payload.channel_id == 807985160703180850 and message.author.id == self.bot.user.id and str(payload.emoji) == "✅":
-        print("Sono scemo!")
-        embed = discord.Embed(description = f"[`Richiesta presa in carico da {member}`]({message.jump_url})", color = discord.Colour.from_rgb(19, 123, 196))
-        await message.edit(content = "", embed = embed)
-        await message.clear_reactions()
-        await message.unpin(reason = f"Richiesta presa in carico da {member}")
+    print("Sono scemo!")
+    guild = self.bot.get_guild(454261607799717888)
+    member = guild.get_user(payload.user_id)
+    channel = guild.get_channel(payload.channel_id)
+    message = await channel.fetch_message(payload.message_id)
+    if member.bot == False and payload.channel_id == 807985160703180850 and message.author.id == self.bot.user.id and str(payload.emoji) == "✅":
+      embed = discord.Embed(description = f"[`Richiesta presa in carico da {member}`]({message.jump_url})", color = discord.Colour.from_rgb(19, 123, 196))
+      await message.edit(content = "", embed = embed)
+      await message.clear_reactions()
+      await message.unpin(reason = f"Richiesta presa in carico da {member}")
 
 def setup(bot):
   bot.add_cog(TicketAlert(bot))
