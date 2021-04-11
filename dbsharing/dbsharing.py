be822918aaf006171bf4d5a07502d8bfe6b47373
Skewer <57370513+sskewer@users.noreply.github.com>
2021-04-11 19:56:49 +0200

    Update dbsharing.py

--- dbsharing/dbsharing.py
+++ dbsharing/dbsharing.py
@@ -11,4 +11,9 @@ class DatabaseSharing(BaseCog):
     """Cog created for local database sharing"""
     def __init__(self, bot):
         self.bot = bot
-        self.epiclink = bot.get_cog("EpicLinking")
+        self.epiclinking = bot.get_cog("EpicLinking")
+        
+        guild = self.bot.get_guild(454261607799717888)
+        verified_user_id = await self.epiclinking.settings.guild(guild).verified_user_id()
+        if str(ctx.message.author.id) in verified_user_id:
+            epic_id = verified_user_id[str(ctx.message.author.id)]
