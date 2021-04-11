aad3c9ed185608f4d7556884c93e8ea1e74c0fb5
Skewer <57370513+sskewer@users.noreply.github.com>
2021-04-11 20:08:15 +0200

    Update dbsharing.py

--- dbsharing/dbsharing.py
+++ dbsharing/dbsharing.py
@@ -7,12 +7,13 @@ from redbot.core.bot import Red
 #---------------# Get Epic Account #---------------#
 
 async def get_epic_account(self, guild, member_id):
-    verified_user_id = await self.epiclinking.settings.guild(guild).verified_user_id()
+    epiclinking = self.bot.get_cog("EpicLinking")
+    verified_user_id = await epiclinking.settings.guild(guild).verified_user_id()
     if str(member_id) in verified_user_id:
         epic_account = { "id": verified_user_id[str(member_id)] }
-        if len(self.epiclinking.clients) > 0:
+        if len(epiclinking.clients) > 0:
             try:
-                epic_user = await self.epiclinking.clients[0].fetch_user(epic_user["id"])
+                epic_user = await epiclinking.clients[0].fetch_user(epic_user["id"])
                 if epic_user is not None:
                     epic_account["name"] = epic_user.display_name
             except:
@@ -30,5 +31,4 @@ class DatabaseSharing(BaseCog):
     """Cog created for local database sharing"""
     def __init__(self, bot):
         self.bot = bot
-        self.epiclinking = bot.get_cog("EpicLinking")
         
