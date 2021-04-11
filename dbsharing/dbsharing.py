11586b956da839d73aeccc4a66bd7cb5606c5a10
Skewer <57370513+sskewer@users.noreply.github.com>
2021-04-11 19:41:56 +0200

    Update dbsharing.py

--- dbsharing/dbsharing.py
+++ dbsharing/dbsharing.py
@@ -8,7 +8,7 @@ from redbot.core.bot import Red
 BaseCog = getattr(commands, "Cog", object)
 
 class DatabaseSharing(BaseCog):
-    
+    """Cog created for local database sharing"""
     def __init__(self, bot):
         self.bot = bot
         self.epiclink = bot.get_cog("EpicLinking")
