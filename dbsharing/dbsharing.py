6538e800d0429ce59ffc8edccc40d5dc235d8bf4
Skewer <57370513+sskewer@users.noreply.github.com>
2021-04-11 19:36:58 +0200

    Create dbsharing.py

---
+++ dbsharing/dbsharing.py
@@ -0,0 +1,13 @@
+import datetime
+import discord
+
+from redbot.core import Config, commands
+from redbot.core.bot import Red
+
+
+BaseCog = getattr(commands, "Cog", object)
+
+class DatabaseSharing(BaseCog):
+    
+    def __init__(self, bot):
+        self.bot = bot
