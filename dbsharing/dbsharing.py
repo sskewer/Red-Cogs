868460840be83abcbf906b4c6264a1d063b2fd01
Skewer <57370513+sskewer@users.noreply.github.com>
2021-04-11 20:16:21 +0200

    Update dbsharing.py

--- dbsharing/dbsharing.py
+++ dbsharing/dbsharing.py
@@ -4,13 +4,14 @@ import discord
 from redbot.core import Config, commands
 from redbot.core.bot import Red
 
+
 #---------------# Get Epic Account #---------------#
 
-async def get_epic_account(self, guild, member_id):
+async def get_epic_account(self, guild, user):
     epiclinking = self.bot.get_cog("EpicLinking")
     verified_user_id = await epiclinking.settings.guild(guild).verified_user_id()
-    if str(member_id) in verified_user_id:
-        epic_account = { "id": verified_user_id[str(member_id)] }
+    if str(user.id) in verified_user_id:
+        epic_account = { "id": verified_user_id[str(user.id)] }
         if len(epiclinking.clients) > 0:
             try:
                 epic_user = await epiclinking.clients[0].fetch_user(epic_user["id"])
@@ -28,6 +29,12 @@ BaseCog = getattr(commands, "Cog", object)
 
 class DatabaseSharing(BaseCog):
     """Cog created for local database sharing"""
+    
     def __init__(self, bot):
         self.bot = bot
+        
+    @commands.guild_only()
+    @commands.command()
+    async def test(self, ctx, member: discord.Member):
+        await ctx.send(f"```json\n{str(get_epic_account(self, ctx.guild, member))}\n```")
         
