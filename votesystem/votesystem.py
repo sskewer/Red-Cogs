f53c579f398c079432c370ba7ac0b4adc13dbae1
Skewer <57370513+sskewer@users.noreply.github.com>
2021-07-28 14:51:38 +0200

    Update votesystem.py

--- votesystem/votesystem.py
+++ votesystem/votesystem.py
@@ -30,20 +30,17 @@ class VoteSystem(BaseCog):
     message = await channel.fetch_message(self.vote_config["message"])
     # Token Check
     token_list = self.mongo.find({})
-    print(token_list)
-    while len([x for x in token_list if x.token == token]) > 0:
+    while len([x for x in list(token_list) if x.token == token]) > 0:
       token = uuid4()
     # User Check
     user_check = self.mongo.find({ "guild": guild.id, "user": payload.member.id })
     # Script
-    if len(user_check) == 0 and payload.member.bot == False and payload.channel_id == channel.id and payload.message_id == message.id and str(payload.emoji) == "✅":
+    if len(list(user_check)) == 0 and payload.member.bot == False and payload.channel_id == channel.id and payload.message_id == message.id and str(payload.emoji) == "✅":
       # Database Update
-      #self.mongo.insert_one({ "guild": str(guild.id), "user": str(payload.member.id), "token": str(token) })
-      print("3")
+      self.mongo.insert_one({ "guild": str(guild.id), "user": str(payload.member.id), "token": str(token) })
       link = self.vote_config["url"] + str(token)
-      print(link)
       # Send DM
-      #await channel.send(link)
+      await channel.send(link)
 
 def setup(bot):
   bot.add_cog(VoteSystem(bot))
