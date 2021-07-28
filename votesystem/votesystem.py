415ec475232673008c28620854394879a596b383
Skewer <57370513+sskewer@users.noreply.github.com>
2021-07-28 11:59:31 +0200

    Update votesystem.py

--- votesystem/votesystem.py
+++ votesystem/votesystem.py
@@ -9,7 +9,7 @@ class VoteSystem(BaseCog):
   def __init__(self, bot):
     self.bot = bot
     self.vote_config = {
-      "url": "https://docs.google.com/forms/d/e/1FAIpQLSc_cZEdmgq23IR8_m6YjbVZTCeCqz2aS8zJ1nrLBWPL0vsmhQ/viewform?usp=pp_url&entry.1831324353=",
+      "url": "https://docs.google.com/forms/d/e/1FAIpQLSc_cZEdmgq23IR8_m6YjbVZTCeCqz2aS8zJ1nrLBWPL0vsmhQ/viewform?usp=pp_url&entry.1831324353={0}",
       "guild": 454261607799717888,
       "channel": 1234,
       "message": 1234
@@ -21,17 +21,14 @@ class VoteSystem(BaseCog):
     token = uuid4()
     # Variables
     guild = self.bot.get_guild(self.vote_config["guild"])
-    member = guild.get_member(payload.user_id)
-    channel = guild.get_channel(payload.channel_id)
-    message = await channel.fetch_message(payload.message_id)
+    channel = guild.get_channel(self.vote_config["channel"])
+    message = await channel.fetch_message(self.vote_config["message"])
     # Database Check
     
     # Script
-    if member.bot == False and payload.channel_id == 807985160703180850 and message.author.id == self.bot.user.id and str(payload.emoji) == "✅":
-      embed = discord.Embed(description = f"[`Richiesta presa in carico da {member}`]({message.jump_url})", color = discord.Colour.from_rgb(19, 123, 196))
-      await message.edit(content = "", embed = embed)
-      await message.clear_reactions()
-      await message.unpin(reason = f"Richiesta presa in carico da {member}")
+    if member.bot == False and payload.channel_id == channel.id and payload.message_id == message.id and str(payload.emoji) == "✅":
+      link = self.vote_config["url"].format(token)
+      # Inviare DM all'utente con il link al voto
 
 def setup(bot):
   bot.add_cog(VoteSystem(bot))
