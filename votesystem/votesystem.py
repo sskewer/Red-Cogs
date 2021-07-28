b13a5fc0938a29b69428379c034dba80eb3a1b21
Skewer <57370513+sskewer@users.noreply.github.com>
2021-07-28 11:27:47 +0200

    Update votesystem.py

--- votesystem/votesystem.py
+++ votesystem/votesystem.py
@@ -1,4 +1,5 @@
 import discord
+from uuid import uuid4
 from redbot.core import commands
 
 BaseCog = getattr(commands, "Cog", object)
@@ -10,10 +11,16 @@ class VoteSystem(BaseCog):
       
   @commands.Cog.listener()
   async def on_raw_reaction_add(self, payload : discord.RawReactionActionEvent):
+    # Token Generation
+    token = uuid4()
+    # Variables
     guild = self.bot.get_guild(454261607799717888)
     member = guild.get_member(payload.user_id)
     channel = guild.get_channel(payload.channel_id)
     message = await channel.fetch_message(payload.message_id)
+    # Database Check
+    
+    # Script
     if member.bot == False and payload.channel_id == 807985160703180850 and message.author.id == self.bot.user.id and str(payload.emoji) == "✅":
       embed = discord.Embed(description = f"[`Richiesta presa in carico da {member}`]({message.jump_url})", color = discord.Colour.from_rgb(19, 123, 196))
       await message.edit(content = "", embed = embed)
