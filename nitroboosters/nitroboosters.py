ddd1c10fe5186c5d26e7488062851cb7c39418bf
Skewer <57370513+sskewer@users.noreply.github.com>
2021-12-04 19:27:25 +0100

    Update nitroboosters.py

--- nitroboosters/nitroboosters.py
+++ nitroboosters/nitroboosters.py
@@ -2,6 +2,15 @@ import discord
 from contextlib import suppress
 from redbot.core import commands
 
+
+#----------------# SETUP #----------------#
+
+nitro_id = 613774322179375105
+colors_id = [778163336910471168, 778164006971113473, 778181408128237628, 778164137184329748, 778164311503667200, 778164647584464896, 778163064364990505, 778164449738489877, 778161810653839360]
+
+#-----------------------------------------#
+
+
 BaseCog = getattr(commands, "Cog", object)
 
 class NitroBoosters(BaseCog):
