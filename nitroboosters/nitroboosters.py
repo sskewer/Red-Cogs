1fbbbb05d96c7df7d72afceafd853a5de342b036
Skewer <57370513+sskewer@users.noreply.github.com>
2021-12-04 19:49:48 +0100

    Update nitroboosters.py

--- nitroboosters/nitroboosters.py
+++ nitroboosters/nitroboosters.py
@@ -5,6 +5,8 @@ from redbot.core import commands
 
 #----------------# SETUP #----------------#
 
+channel_id = 778165263928655882
+message_id = 778180901678219266
 nitro_id = 613774322179375105
 colors_id = [778163336910471168, 778164006971113473, 778181408128237628, 778164137184329748, 778164311503667200, 778164647584464896, 778163064364990505, 778164449738489877, 778161810653839360]
 
@@ -22,6 +24,8 @@ class NitroBoosters(BaseCog):
   @commands.Cog.listener()
   async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
     if not guild:
+      return
+    if payload.channel_id != channel_id or payload.message_id == message_id:
       return
     # Vars
     member = guild.get_member(payload.user_id)
