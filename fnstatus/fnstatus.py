86c06b74869695452e266009d374a66a04befa7e
Skewer <57370513+sskewer@users.noreply.github.com>
2021-12-29 21:25:25 +0100

    Update fnstatus.py

--- fnstatus/fnstatus.py
+++ fnstatus/fnstatus.py
@@ -2,7 +2,8 @@ import asyncio
 import datetime
 import discord
 
-from redbot.core import commands
+from discord.ext import tasks
+from redbot.core import Config, commands
 from twitter_scraper import get_tweets
 from translate import Translator
 
@@ -13,6 +14,22 @@ class FnStatus(BaseCog):
     def __init__(self, bot):
         self.bot = bot
         self.translator = Translator(to_lang="it")
+        self.config = Config.get_conf(self, identifier=4000121212121212, force_registration=True)
+        default_guild = {"last_feed": None}
+        self.config.register_guild(**default_guild)
+        self.checker.start()
+        
+    def cog_unload(self):
+        self.checker.cancel()
+        
+    @tasks.loop(minutes=10, reconnect=True)
+    async def checker(self):
+        guild = self.bot.get_guild(454261607799717888)
+        last_feed = await self.config.guild(guild).last_feed() 
+                                  
+    @checker.before_loop
+    async def before_checker(self):
+        await self.bot.wait_until_ready()
         
         # self.translator.translate("This is a pen.")
         
