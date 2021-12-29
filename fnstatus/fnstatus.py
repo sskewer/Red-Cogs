46344c2b3857942d447400d4738f8ed61c20d7e3
Skewer <57370513+sskewer@users.noreply.github.com>
2021-12-29 21:10:45 +0100

    Update fnstatus.py

--- fnstatus/fnstatus.py
+++ fnstatus/fnstatus.py
@@ -1,1 +1,15 @@
+import asyncio
+import datetime
+import discord
 
+from redbot.core import commands
+from twitter_scraper import get_tweets
+
+BaseCog = getattr(commands, "Cog", object)
+
+class FnStatus(BaseCog):
+    """Inviare tweet tradotti in italiano degli account Twitter di Fortnite"""
+    def __init__(self, bot):
+        self.bot = bot
+        
+    
