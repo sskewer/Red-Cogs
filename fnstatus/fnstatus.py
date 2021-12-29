7e746c13d71d267c721dce97fcfa3452a71fe007
Skewer <57370513+sskewer@users.noreply.github.com>
2021-12-29 21:14:57 +0100

    Update fnstatus.py

--- fnstatus/fnstatus.py
+++ fnstatus/fnstatus.py
@@ -4,6 +4,7 @@ import discord
 
 from redbot.core import commands
 from twitter_scraper import get_tweets
+from translate import Translator
 
 BaseCog = getattr(commands, "Cog", object)
 
@@ -11,5 +12,12 @@ class FnStatus(BaseCog):
     """Inviare tweet tradotti in italiano degli account Twitter di Fortnite"""
     def __init__(self, bot):
         self.bot = bot
+        self.translator = Translator(to_lang="it")
+        
+        # self.translator.translate("This is a pen.")
+        
+        
+        #for tweet in get_tweets('FortniteStatus', pages=1):
+        #print(tweet['text'])
         
     
