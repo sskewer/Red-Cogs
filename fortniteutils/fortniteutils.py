1c2ae688c3acc76deaf35483c85b152bbef66eb9
Skewer <57370513+sskewer@users.noreply.github.com>
2022-07-18 17:36:42 +0200

    Update fortniteutils.py

--- fortniteutils/fortniteutils.py
+++ fortniteutils/fortniteutils.py
@@ -142,12 +142,18 @@ class FortniteUtils(BaseCog):
     fn_api = fortnite_api.FortniteAPI(api_key=(await self.bot.get_shared_api_tokens('FortniteAPI'))['api_key'], run_async=True)
     # Getting Data
     try:
-      if gamemode == "stw":
+      if gamemode == "Salva il Mondo":
         news_type = fortnite_api.NewsType.SAVE_THE_WORLD
-      elif gamemode == "cr":
+        title = "Salva il Mondo"
+        icon = "https://cdn.discordapp.com/emojis/775676864055214080.png"
+      elif gamemode == "Creativa":
         news_type = fortnite_api.NewsType.CREATIVE
+        title = "Creativa"
+        icon = "https://cdn.discordapp.com/emojis/775676864664305674.png"
       else:
         news_type = fortnite_api.NewsType.BATTLE_ROYALE
+        title = "Battaglia Reale"
+        icon = "https://cdn.discordapp.com/emojis/775676863976046592.png"
       news = await fn_api.news.fetch_by_type(news_type=news_type, language=fortnite_api.GameLanguage.ITALIAN)
     except:
       return await inter.reply(f"😕 Ops... qualcosa è andato storto!", ephemeral=True)
@@ -158,6 +164,7 @@ class FortniteUtils(BaseCog):
       return await inter.reply(f"😕 Ops... qualcosa è andato storto!", ephemeral=True)
     # Response
     embed = discord.Embed(timestamp=date)
+    embed.set_author(name=f"Notizie: {title}", icon=icon)
     embed.set_image(url=url)
-    embed.set_footer(text="Creato con ❤️ · Fortnite IT", icon_url=fn_api_icon)
+    embed.set_footer(text="🕓 Notizie aggiornate a ", icon_url=fn_api_icon)
     await inter.reply(embed=embed, ephemeral=True)
