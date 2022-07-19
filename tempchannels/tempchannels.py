4a4762561b27d9a1648f2f72694d419c0d55f427
Skewer <57370513+sskewer@users.noreply.github.com>
2022-07-19 11:31:58 +0200

    Update tempchannels.py

--- tempchannels/tempchannels.py
+++ tempchannels/tempchannels.py
@@ -24,19 +24,26 @@ class TempChannels(BaseCog):
 
   @commands.Cog.listener()
   async def on_voice_state_update(self, member, before, after):
+    # Channel Creation
     if (before.channel is None or before.channel.id != main_channel) and after.channel.id == main_channel:
       user_channel = await self.config.member(member).channel()
       # Existing User Channel
       if user_channel is not None:
         return await member.move_to(self.bot.get_channel(user_channel))
       # Non-Existing User Channel
-      channel = await member.guild.create_voice_channel(name=member.display_name, user_limit=4, category=self.bot.get_channel(main_category))
+      channel = await member.guild.create_voice_channel(name=member.display_name, topic=f"👤 **Proprietario: {member.mention}**", user_limit=4, category=self.bot.get_channel(main_category))
       await member.move_to(channel)
       await self.config.member(member).channel.set(channel.id)
       #add user to voice-commands
-
-    #if before.channel != None and after.channel == None:
-      #if before.channel.category_id == main_category:
-        #if len(before.channel.members) == 0:
-          #await channel.delete()
+      
+    # Channel Elimination
+    if before.channel is not None and before.channel.category_id == main_category:
+      if len(before.channel.members) < 1:
+        try:
+          member = before.channel.guild.get_member(before.channel.name[21:-3])
+        except:
+          member = None
+        if member is not None:
+          await self.config.member(member).channel.set(None)
+        await before.channel.delete()
 
