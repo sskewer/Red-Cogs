2063a10c727ae8eb131ea8aa971355c34d454b85
Skewer <57370513+sskewer@users.noreply.github.com>
2022-07-19 10:15:51 +0200

    Update fortniteutils.py

--- fortniteutils/fortniteutils.py
+++ fortniteutils/fortniteutils.py
@@ -212,7 +212,7 @@ class FortniteUtils(BaseCog):
         if index < len(pages):
           row_data["components"][1]["disabled"] = False
         await menu.edit(embed=pages[index-1], components=[ActionRow.from_dict(row_data)])
-        return await click.defer()
+        return await click.create_response("")
 
       @on_click.matching_id(f"menu_{str(inter.author.id)}_next")
       async def on_next_button(click):
@@ -223,14 +223,14 @@ class FortniteUtils(BaseCog):
         if index == len(pages):
           row_data["components"][1]["disabled"] = True
         await menu.edit(embed=pages[index-1], components=[ActionRow.from_dict(row_data)])
-        return await click.defer()
+        return await click.create_response("")
 
       @on_click.matching_id(f"menu_{str(inter.author.id)}_close")
       async def on_close_button(click):
         new_embed = click.message.embeds[0].to_dict()
         new_embed["title"] = click.message.embeds[0].title[:-6]
         await menu.edit(embed=discord.Embed.from_dict(new_embed), components=[])
-        return await click.defer()
+        return await click.create_response("")
 
       @on_click.timeout
       async def on_timeout():
