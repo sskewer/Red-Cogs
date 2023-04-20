f4cb67392f369b72f7e9c0ea2cf5a22eacc620b2
Skewer <57370513+sskewer@users.noreply.github.com>
2023-04-20 15:17:05 +0200

    Update tweetrepost.py

--- tweetrepost/tweetrepost.py
+++ tweetrepost/tweetrepost.py
@@ -1,4 +1,5 @@
 import tweepy
+import asyncio
 import requests
 import dateutil.parser
 
@@ -19,67 +20,71 @@ class TweetRepost(BaseCog):
     super().__init__(*args, **kwargs)
     self.bot = bot
     self.config = Config.get_conf(self, identifier=4000121212000335, force_registration=True)
-    default_guild = {"last_id": 0}
-    self.config.register_guild(**default_guild)
+    default_global = {"last_id": 0}
+    self.config.register_global(**default_global)
+    self.bot.loop.create_task(self.look_for_new_tweets())
         
-  @commands.Cog.listener()
-  async def on_message(self, message):
-    try:
-      api_tokens = await bot.get_shared_api_tokens('TweetRepost')
-    except:
-      return
-    auth = tweepy.OAuth1UserHandler(
-      api_tokens["consumer_key"], 
-      api_tokens["consumer_secret"], 
-      api_tokens["access_token"], 
-      api_tokens["access_token_secret"]
-    )
-    api = tweepy.API(auth)
-    tweets = api.search_tweets(f"from:{str(api_tokens["tweet_user"])}", count = 10, tweet_mode = "extended")
-    to_post = []
-    for tweet in tweets:
-      # Getting tweet data
-      input_data = tweet._json
-      # Getting image
+  async def look_for_new_tweets(self):
+    while True:
+
       try:
-        image = input_data["entities"]["media"][0]["media_url"]
-      except IndexError:
-        image = None
-      # Extracting important data
-      to_post.append({
-        "id": tweet.id,
-        "text": input_data["full_text"],
-        "image": image,
-        "timestamp": dateutil.parser.parse(input_data["created_at"]).timestamp(),
-      })
-    # Last posted tweet check
-    last_id = await self.config.guild(self.guild).last_id()
-    to_post.sort(key = lambda x: x['timestamp'])                               
-    try:
-      index = [t["id"] for t in to_post].index(last_id)
-    except ValueError:
-      index = -1
-    if index != -1:
-      to_post = to_post[index+1:]
-    # Webhook Posts
-    for post in to_post:
-      try:
-        requests.post(
-          "https://Fortnite.mettiushyper.repl.co/webhook",
-          headers = {
-            "webhook_url": api_tokens["webhook_url"],
-          },
-          json = {
-            "text": post["text"],
-            "image": post["image"],
-            "timestamp": post["timestamp"],
-            "color": "#ffffff",
-            "translate_language": "it",
-          },
-        )
-        await self.config.guild(self.guild).last_id.set(to_post["id"])
+        api_tokens = await bot.get_shared_api_tokens('TweetRepost')
       except:
-        pass
+        return
+      auth = tweepy.OAuth1UserHandler(
+        api_tokens["consumer_key"], 
+        api_tokens["consumer_secret"], 
+        api_tokens["access_token"], 
+        api_tokens["access_token_secret"]
+      )
+      api = tweepy.API(auth)
+      tweets = api.search_tweets(f"from:{str(api_tokens["tweet_user"])}", count = 10, tweet_mode = "extended")
+      to_post = []
+      for tweet in tweets:
+        # Getting tweet data
+        input_data = tweet._json
+        # Getting image
+        try:
+          image = input_data["entities"]["media"][0]["media_url"]
+        except IndexError:
+          image = None
+        # Extracting important data
+        to_post.append({
+          "id": tweet.id,
+          "text": input_data["full_text"],
+          "image": image,
+          "timestamp": dateutil.parser.parse(input_data["created_at"]).timestamp(),
+        })
+      # Last posted tweet check
+      last_id = await self.config.last_id()
+      to_post.sort(key = lambda x: x['timestamp'])                               
+      try:
+        index = [t["id"] for t in to_post].index(last_id)
+      except ValueError:
+        index = -1
+      if index != -1:
+        to_post = to_post[index+1:]
+      # Webhook Posts
+      for post in to_post:
+        try:
+          requests.post(
+            "https://Fortnite.mettiushyper.repl.co/webhook",
+            headers = {
+              "webhook_url": api_tokens["webhook_url"],
+            },
+            json = {
+              "text": post["text"],
+              "image": post["image"],
+              "timestamp": post["timestamp"],
+              "color": "#ffffff",
+              "translate_language": "it",
+            },
+          )
+          await self.config.last_id.set(to_post["id"])
+        except:
+          pass
+        
+      await asyncio.sleep(900)  # pause for 15 minutes
 
 
 def setup(bot):
